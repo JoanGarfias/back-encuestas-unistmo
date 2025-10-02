9099e2400db1ffb946840b4484adf220ce90a379
Joan <113860964+JoanGarfias@users.noreply.github.com>
2025-10-02 14:24:19 -0600

    project init

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def inicio():
+    datos = {
+        "mensaje": "¡Bienvenido a tu API de Flask!",
+        "nombre_usuario": "Lucía",
+        "tipo_respuesta": "json"
+    }
+    return jsonify(datos)
