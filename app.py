ec76c97a660f2499765318bf0731a617ac1d1eed
JuanEEE <jemmanuelbn2@gmail.com>
2025-10-05 18:32:08 -0600

    Correcciones al modulo de crear registros
    El proceso sigue siendo el mismo:
    1. Crear una base de datos Mysql con un gestor (xampp y phpmyadmin fueron los que use)
    2. Poblar los datos necesarios en el codigo, lineas 6 a 9
    3. Ir a la ruta /tabla en el navegador ya lanzada la app con flask para que se cree la tabla si no existe, si ya existe saltarse este paso
    4. En un body con formato application/JSON enviar los datos del usuario en el formato especificado en las lineas 52 a 67
    5. profit

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy # ejecutar pip install flask flask_sqlalchemy pymysql en la consola si no se tiene de antemano el flask_sqlalchemy
 from urllib.parse import quote_plus
+from datetime import date
 
-# Datos de la base
-usuario = 'dummy'
+# Datos de la base (rellenar con datos correctos si se van a hacer pruebas)
+usuario = 'panela'
 ruta = 'localhost'
-base = 'dummy'
-password = 'nPS@/zo(_2Qm175O'
+base = 'queso'
+password = 'F6g]x4WcP[oagFxD'
 encoded_password = quote_plus(password)
 
 
@@ -19,19 +20,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    matricula = db.Column(db.Integer, nullable=False)
-    nombre = db.Column(db.String(100), nullable=False)
+class Encuestas(db.Model):
+    id_u = db.Column(db.Integer, primary_key=True)
+    carrera = db.Column(db.String(128), nullable=False)
+    nombre = db.Column(db.String(255), nullable=False)
     edad = db.Column(db.Integer, nullable=False)
-    carrera = db.Column(db.Integer, nullable=False)
+    sexo = db.Column(db.Enum("M", "F"), nullable=False)
     semestre = db.Column(db.Integer, nullable=False)
-    estatura = db.Column(db.Integer, nullable=False)
-    promedio = db.Column(db.Float, nullable=False)
-    discapacidad = db.Column(db.Boolean, nullable=False)
-    trabaja = db.Column(db.Boolean, nullable=False)
-    gasto = db.Column(db.Float, nullable=False)
-    rol = db.Column(db.Integer, nullable=False)
+    fecha_registro = db.Column(db.Date, nullable=False)
+    promedio_anterior = db.Column(db.Float, nullable=False)
+    tiempo_traslado = db.Column(db.Integer, nullable=False)
+    trabaja = db.Column(db.Integer, nullable=False)
+    gasto_mensual = db.Column(db.Float, nullable=False)
+    discapacidad = db.Column(db.Integer, nullable=False)
+    peso = db.Column(db.Float, nullable=False)
+    altura = db.Column(db.Integer, nullable=False)
+    correo = db.Column(db.String(128), nullable=False, unique=True)
 
 @app.route('/')
 
@@ -47,17 +51,18 @@ def inicio():
 def recibirDatos():
     """ Formato esperado del JSON en el body
         {
-        "matricula": int,
+        "carrera": string,
         "nombre": string,
         "edad": int,
-        "carrera": int, opciones disponibles: 1 quimica,2 petroleos,3 diseño,4 computacion,7 industrial,10 matematicas,16 renovables
+        "sexo": 'M', 'F'
         "semestre": int, opciones disponibles: 1, 3, 5, 7, 9 o 2, 4, 6, 8, 10 (solo pares o solo impares, no mezclados)
-        "estatura": int,
-        "promedio": double,
-        "discapacidad": bool,
-        "trabaja": bool,
-        "gasto": double,
-        "rol": int opciones disponibles: 0 alumno, 1 maestro
+        "promedio_anterior": double,
+        "tiempo_traslado": int,
+        "trabaja": int,
+        "gasto_mensual": double,
+        "discapacidad": int,
+        "peso": double,
+        "altura": int,
         }
         """
 
@@ -71,47 +76,77 @@ def recibirDatos():
             return error("Sin datos en el body.")
 
         # Validar carrera
-        if data['carrera'] not in [1, 2, 3, 4, 7, 10, 16]:
+        if not data['carrera']:
             return error("Carrera no válida.")
+
+        #Validar nombre
+        if not data['nombre']:
+            return error("Nombre no válido.")
+
+        # Validar edad
+        if(data["edad"] < 0 or data["edad"] > 100):
+            return error("Edad no válida.")
+
+        # Validar sexo
+        if data['sexo'] not in ["M", "F"]:
+            return error("Sexo no válido.")
 
         # Validar semestre
         if data['semestre'] not in [1, 3, 5, 7, 9] and data['semestre'] not in [2, 4, 6, 8, 10]:
             return error("Semestre no válido.")
 
-        # Validar estatura
-        if(data['estatura'] < 100 or data['estatura'] > 230):
-            return error("Estatura no válida.")
-
-        # Validar promedio
-        if(data['promedio'] < 0.0 or data['promedio'] > 10.0):
+        # Validar promedio anterior
+        if(data["promedio_anterior"] < 0.0 or data["promedio_anterior"] > 10.0):
             return error("Promedio no válido.")
 
-        # Validar rol
-        if data['rol'] not in [0,1]:
-            return error("Rol no válido.")
-        
+        # Validar altura
+        if(data['altura'] < 100 or data['altura'] > 230):
+            return error("Altura no válida.")
+
+        # Validar tiempo_traslado
+        if(data["tiempo_traslado"] < 0 or data["tiempo_traslado"] > 300):
+            return error("Tiempo de traslado no válido.")
+
+        # Validar si trabaja
+        if data['trabaja'] not in [0,1]:
+            return error("Opcion de trabajo no válida.")
+
+        # Validar peso
+        if(data['peso'] < 40 or data['peso'] > 200):
+            return error("Peso no válido.")
+
+        # Validar altura
+        if(data['altura'] < 100 or data['altura'] > 230):
+            return error("Altura no válida.")
+
+        #Validar correo
+        if(not data["correo"]):
+            return error("No se permite un correo vacio.")
 
         try:
         # Crear un usuario
-            user = User(
-            matricula=data["matricula"],
+            encuestas = Encuestas(
+            carrera=data["carrera"],
             nombre=data["nombre"],
             edad=data["edad"],
-            carrera=data["carrera"],
+            sexo=data["sexo"],
             semestre=data["semestre"],
-            estatura=data["estatura"],
-            promedio=data["promedio"],
-            discapacidad=data["discapacidad"],
+            fecha_registro=date.today(),
+            promedio_anterior=data["promedio_anterior"],
+            tiempo_traslado=data["tiempo_traslado"],
             trabaja=data["trabaja"],
-            gasto=data["gasto"],
-            rol=data["rol"]
+            gasto_mensual=data["gasto_mensual"],
+            discapacidad=data["discapacidad"],
+            peso=data["peso"],
+            altura=data["altura"],
+            correo=data["correo"]
         )
         except Exception as e:
             return error(f"Ocurrió un error al crear el usuario: {str(e)}")
         
         # Agregar el usuario a la base 
         try:
-            db.session.add(user)
+            db.session.add(encuestas)
             db.session.commit()
         except Exception as e:
             db.session.rollback()  # Deshacer cambios si ocurrio un error
@@ -124,12 +159,12 @@ def recibirDatos():
 def inicio3():
     with app.app_context():
         # Inspeccionar la base
-        inspector = inspect(db.engine)
+        #inspector = inspect(db.engine)
         
-        # Ver si la tabla 'user' existe
-        if 'user' not in inspector.get_table_names():
-            db.create_all()  # Crea la tabla si no existe
-            return jsonify({"status": "success", "mensaje": "Tabla creada!"}), 201
-        else:
-            print("Tabla ya existe.")
-            return jsonify({"status": "success", "mensaje": "La tabla ya existe!"}), 200
+        # Ver si la tabla 'encuestas' existe
+        #if 'encuestas' not in inspector.get_table_names():
+        db.create_all()  # Crea la tabla si no existe
+        return jsonify({"status": "success", "mensaje": "Tabla creada!"}), 201
+        #else:
+        #    print("Tabla ya existe.")
+        #    return jsonify({"status": "success", "mensaje": "La tabla ya existe!"}), 200
